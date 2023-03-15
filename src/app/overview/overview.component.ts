0bfe4b859ab0da58b24a5e5dbc2f894cc8dd854d
Darren Sapalo <darren.sapalo@gmail.com>
2023-03-15 15:59:17 +0800

    Added tweet embedding integration on home page.

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./overview.component.sass']
 })
 export class OverviewComponent {
-
+  ngAfterViewInit(): void {
+      (<any>window).twttr.widgets.load();
+  }
 }
