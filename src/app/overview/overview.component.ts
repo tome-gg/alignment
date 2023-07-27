23027d00ba4e3e1f697cb96479ea60af42e80e2e
Darren Sapalo <darren.sapalo@gmail.com>
2023-07-27 13:14:24 +0800

    Improvements on mobile responsiveness.

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
+import { getGtagFn } from 'ngx-google-analytics';
 
-var gtag: any;
-
+const gtag = getGtagFn(window, []);
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
@@ -16,19 +16,24 @@ export class OverviewComponent {
     (window as any).Tawk_API.popup();
   }
 
-  selectService(serviceType: 'find_a_mentor'|'1on1'|'group'|'direction_discovery'|'rapid'|'interactive_case_studies') {
+  selectService(event: Event, serviceType: 'find_a_mentor'|'1on1'|'group'|'direction_discovery'|'rapid'|'interactive_case_studies') {
+    event.preventDefault();
     this.serviceSelected = true;
     this.isOpen = false;
+    
     gtag('event', 'SHOW_INTEREST', {
       'event_category': 'SERVICES',
       'event_label': serviceType
+     });
+
+     window.scrollTo({
+      top: 0
      })
   }
 
   track(eventName: string) {
     gtag('event', eventName, {
     'event_category': 'BUTTON_CLICK' })
-    
   }
 
   ngAfterViewInit(): void {
