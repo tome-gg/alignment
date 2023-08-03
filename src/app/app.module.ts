5fea79d172987db9444cb54747a01c29e6fa1b07
Darren Sapalo <darren.sapalo@gmail.com>
2023-08-03 08:44:42 +0800

    WIP for auth0 integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,28 @@ import { AppComponent } from './app.component';
 import { TrainingComponent } from './training/training.component';
 import { OverviewComponent } from './overview/overview.component';
 import { CommonModule, NgIf } from '@angular/common';
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrainingComponent,
     OverviewComponent,
+    AuthButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonModule,
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'dev-hhqarv2odubqjclm.us.auth0.com',
+      clientId: '5qJ8mQjcqUeXX8Jue3O70w10J7fkQFTS',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
     NgIf,
   ],
   providers: [],
