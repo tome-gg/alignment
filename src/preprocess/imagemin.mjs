f38dd13235acdb4f6c1526008fa16fbc3524df1d
Darren Sapalo <darren.sapalo@gmail.com>
2023-11-06 07:57:12 -0800

    Minified the web assets

--- src/preprocess/imagemin.mjs
+++ src/preprocess/imagemin.mjs
@@ -1,7 +1,11 @@
 import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
 
-await imagemin(['./src/assets/*.{jpg,png}'], {
+await imagemin([
+	'./src/assets/*.{jpg,png}',
+	'./src/assets/books/*.{jpg,png}',
+	'./src/assets/growth/*.{jpg,png}'
+], {
 	destination: './src/assets',
 	plugins: [
 		imageminWebp({quality: 50})
