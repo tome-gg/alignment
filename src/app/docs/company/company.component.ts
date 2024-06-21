7dc8dd1ff13215e06ca2e479417fc2c5425eca27
Darren Sapalo <darren.sapalo@gmail.com>
2024-06-20 18:05:44 -0700

    Updated experience design of reading about the company.

--- src/app/docs/company/company.component.ts
+++ src/app/docs/company/company.component.ts
@@ -18,6 +18,30 @@ export class CompanyComponent implements OnInit {
     return this.counter;
   }
 
+  next(count: number) {
+    this.minCounter = count;
+    let section = '';
+    switch (count) {
+      case 1: section = 'sec_academics'; break;
+      case 2: section = 'sec_all_work_no_play'; break;
+      case 3: section = 'sec_lack_of_confidence'; break;
+      case 4: section = 'sec_communication_gap'; break;
+      case 5: section = 'sec_personalization'; break;
+      case 6: section = 'sec_team'; break;
+    }
+
+    setTimeout(() => {
+      const elem = document.getElementById(section);
+      if (elem) {
+        elem.scrollIntoView({ behavior: 'smooth' });
+        console.log('scrlling')
+      }
+      else {
+        console.error("where wtf")
+      }
+    }, 1000)
+  }
+
   ngOnInit() {
     // How long before each message is sent over
     const delayInSec = 12;
