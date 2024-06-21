5d6e88b5d9571a585954e7a1b8ce3a83136a0d89
Darren Sapalo <darren.sapalo@gmail.com>
2024-06-20 17:54:42 -0700

    Updated slow display of the about page

--- src/app/docs/company/company.component.ts
+++ src/app/docs/company/company.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.sass']
 })
-export class CompanyComponent {
+export class CompanyComponent implements OnInit {
 
+  counter = 0;
+  minCounter = 0;
+
+  get minimumDisplayCounter() {
+    if (this.minCounter >= this.counter) {
+      return this.minCounter;
+    }
+    return this.counter;
+  }
+
+  ngOnInit() {
+    // How long before each message is sent over
+    const delayInSec = 4;
+
+    const messages$ = interval(delayInSec * 1000).pipe(
+      filter((v) => v !== null && v !== undefined),
+      map((_, idx) => idx)
+    );
+
+    messages$.subscribe((cnt) => {
+      this.counter = cnt;
+      console.log(cnt);
+    });
+  }
 }
