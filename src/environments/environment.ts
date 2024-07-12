b6ee69d18471075c081fac6637f7a9836eb28841
Darren Sapalo <darren.sapalo@gmail.com>
2024-07-12 01:45:49 -0700

    Loads environment variables correctly

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  appVersion: '1.0.0',
   auth: {
     domain: 'dev-phh2u7lm3h45n2fy.us.auth0.com',
     clientId: 'xOWUb7eGFmKmdCobWEcpGswGEI87EL84',
---
+++ src/environments/set-env.ts
@@ -0,0 +1,40 @@
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+// Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+// Load node modules
+  const colors = require('colors');
+  const appVersion = require('../../package.json').version;
+  require('dotenv').config({
+    path: 'src/environments/.env'
+  });
+
+// `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  production: false,
+  appVersion: '${appVersion}',
+  auth: {
+    domain: '${process.env['AUTH0_DOMAIN']}',
+    clientId: '${process.env['AUTH0_CLIENT_ID']}',
+    cacheLocation: 'localstorage',
+  },
+  hasura: {
+    graphql: '${process.env['HASURA_GRAPHQL_URL']}',
+    wss: '${process.env['HASURA_WSS_URL']}',
+    api: '${process.env['HASURA_API_URL']}'
+  }
+};
+`;
+  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  writeFile(targetPath, envConfigFile, (err: any) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+  });
+};
+
+setEnv();
